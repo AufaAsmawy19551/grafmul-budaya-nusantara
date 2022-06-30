7dc47d3b3e85eb49ad44cbf90fcd4b89fc7bc12c
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-06-30 18:05:00 +0700

    test

--- Main/out/source/Main.java
+++ Main/out/source/Main.java
@@ -13,16 +13,40 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 
-package Main;
+public class Main extends PApplet {
 
 
-void setup() {
-  size(600, 400);
+ public void setup() {
+  /* size commented out by preprocessor */;
     background(255, 255, 0);
 }
 
-void draw(){
-Person person = new Person();
+ public void draw(){
+Person.Guru person = new Person.Guru();
 person.drawBody();
 }
 
+
+// import processing.pdf.*;
+
+// void setup() {
+//   size(1024, 768, PDF);
+// }
+
+// void draw() {
+//   line(0, 0, width, height);
+//   line(0, height, width, 0);
+// }
+
+
+  public void settings() { size(600, 400); }
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Main" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ Main/test/Person.pde
@@ -0,0 +1,23 @@
+public class Person  {
+  
+  public Person() {
+    
+  }
+  
+  public void drawBody() {
+    rect(0, 0, 300, 300);
+  }
+  
+  public void drawHair() {
+    
+  }
+  
+  public void drawEye() {
+    
+  }
+  
+  public void drawMouth() {
+    
+  }
+  
+}
