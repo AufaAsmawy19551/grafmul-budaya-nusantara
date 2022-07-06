c3cda1fe98ca5ec65f99816699214ba37c789cd4
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-07-06 21:05:31 +0700

    test

--- Main/out/source/Main.java
+++ Main/out/source/Main.java
@@ -115,9 +115,8 @@ public class RumahAceh extends Rumah {
     translate(x, y);
     scale(s);
     
-    noStroke();
-    
     //atap
+    noStroke();
     fill(0xFFB5B3B4);
     triangle(470, 500, 960, 190, 1450, 500);
     fill(0xFF9D6063);
@@ -249,6 +248,130 @@ public class RumahAceh extends Rumah {
     rect(1160, y, 100, 10);
   }
   
+}
+
+public class RumahGadang extends Rumah {
+  
+  public RumahGadang() {
+    
+  }
+  
+  public void draw(float x, float y, float z, float s) {
+    
+    translate(x, y);
+    scale(s);
+    
+    //badan depan
+    noStroke();
+    beginShape();
+    fill(196, 33, 33);
+    vertex(620, 540);
+    vertex(1395, 540);
+    vertex(1395, 720);
+    vertex(620, 720);
+    endShape();
+    //badan samping kiri
+    beginShape();
+    fill(163, 26, 26);
+    vertex(435, 490);
+    vertex(620, 490);
+    vertex(620, 720);
+    vertex(435, 690);
+    endShape();
+    //badan samping kanan
+    beginShape();
+    fill(163, 26, 26);
+    vertex(1395, 490);
+    vertex(1580, 490);
+    vertex(1580, 690);
+    vertex(1395, 720);
+    endShape();
+    
+    //jendela kiri
+    fill(43, 35, 35);
+    rect(645, 620, 35 ,55);
+    rect(730, 625, 35 ,55);
+    rect(818, 632, 35 ,55);
+    rect(905, 636, 35 ,55);
+    //jendela kanan
+    rect(1078, 636, 35 ,55);
+    rect(1165, 632, 35 ,55);
+    rect(1250, 625, 35 ,55);
+    rect(1335, 620, 35 ,55);
+    
+    
+    //atap kiri 1
+    beginShape();
+    fill(77, 70, 70);
+    vertex(370, 170);
+    quadraticVertex(445, 320, 545, 400);
+    vertex(660, 545);
+    quadraticVertex(520, 555, 380, 490);
+    stroke(0xFFFFFFFF);
+    strokeWeight(5);       
+    quadraticVertex(400, 330, 370, 170);   
+    endShape();
+    //atap kiri 2
+    beginShape();
+    stroke(0xFFFFFFFF);
+    strokeWeight(5);
+    fill(77, 70, 70);
+    vertex(460, 185);
+    quadraticVertex(555, 380, 610, 380);
+    vertex(610, 515);
+    vertex(480, 490);
+    quadraticVertex(500, 365, 460, 185);
+    endShape();
+    
+    //atap kanan 1
+    beginShape();
+    stroke(0xFFFFFFFF);
+    strokeWeight(5);
+    fill(77, 70, 70);
+    vertex(1640, 170);
+    quadraticVertex(1576, 320, 1484, 382);
+    vertex(1338, 545);
+    quadraticVertex(1490, 557, 1640, 490);
+    quadraticVertex(1601, 330, 1640, 170);
+    endShape();
+    
+    //atap kanan 2
+    beginShape();
+    stroke(0xFFFFFFFF);
+    strokeWeight(5);
+    fill(77, 70, 70);
+    vertex(1557, 185);
+    quadraticVertex(1500, 370, 1535, 490);
+    vertex(1390, 515);
+    vertex(1390, 380);
+    quadraticVertex(1470, 380, 1557, 185);
+    endShape();
+    
+    //atap tengah atas
+    beginShape();
+    stroke(0xFFFFFFFF);
+    strokeWeight(5);
+    fill(77, 70, 70);
+    vertex(575, 215);
+    quadraticVertex(1005, 700, 1435, 215);
+    quadraticVertex(1385, 430, 1435, 565);
+    quadraticVertex(1005, 625, 575, 565);
+    quadraticVertex(610, 410, 575, 215);
+    endShape();
+    
+    //atap tengah atas
+    beginShape();
+    stroke(0xFFFFFFFF);
+    strokeWeight(5);
+    fill(77, 70, 70);
+    vertex(760, 260);
+    quadraticVertex(1002, 400, 1245, 260);
+    quadraticVertex(1220, 357, 1245, 455);
+    quadraticVertex(1002, 505, 760, 455);
+    quadraticVertex(785, 360, 760, 260);
+    endShape();
+    
+  }
 }
 
 public class RumahJateng extends Rumah {
