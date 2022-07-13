747c9e535490bd1e0944cd5ac2df3d6da88be167
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-07-13 23:50:00 +0700

    test

--- Main/out/source/Main.java
+++ Main/out/source/Main.java
@@ -1198,7 +1198,7 @@ public class PropertiPesawat extends Object {
     super(x, y, s);
   }
 
-  public void draw(){
+  public void draw(String daerah){
     pushMatrix();
     
     translate(this.position[0], this.position[1]);
@@ -1213,6 +1213,25 @@ public class PropertiPesawat extends Object {
     strokeJoin(ROUND);
     triangle(50, 41, 60, 41, 55, 37);
     triangle(50, 57, 60, 57, 55, 65);
+    
+    popMatrix();
+
+    pushMatrix();
+    
+    translate(this.position[0], this.position[1]);
+    scale(this.s);
+    
+    strokeWeight(2);
+    line(90, 50, 200, 60);
+    rect(150, 40, 400, 30);
+    triangle(150, 40, 150, 50, 145, 40);
+    triangle(150, 60, 150, 70, 145, 70);
+    triangle(550, 40, 550, 50, 555, 40);
+    triangle(550, 60, 550, 70, 555, 70);
+    
+    fill(255);
+    textSize(17);
+    text("Rumah Adat " + daerah, 170, 60);
     
     popMatrix();
   }
